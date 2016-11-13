238dbfa5b6c505813d6188bc604e23994c1ff02e
Louis Silvestro <silvel2@rpi.edu>
2016-11-12 21:21:17 -0500

    Add base from Nest thermostat support

---
+++ src/homePowerSaver/NestWrapper.java
@@ -0,0 +1,11 @@
+package homePowerSaver;
+/*Wrapper for using the Nest Thermostat API*/
+public class NestWrapper {
+	private int temp;
+	private final String uri = "";
+	
+	public NestWrapper() {
+	
+	}
+	
+}
