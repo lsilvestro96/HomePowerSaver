f3b2c6e73f6b4975e0a7dfc71ed22287a6fb1c99
Louis Silvestro <silvel2@rpi.edu>
2016-11-13 12:20:42 -0500

    Added more code for nest support

--- src/homePowerSaver/NestWrapper.java
+++ src/homePowerSaver/NestWrapper.java
@@ -1,7 +1,10 @@
 package homePowerSaver;
 import java.io.IOException;
+
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 
 /*Wrapper for using the Nest Thermostat API*/
@@ -13,7 +16,31 @@ public class NestWrapper {
 		this.accessToken = accessToken;
 	}
 	
-	public void enablePowerSave() {
+	public void enablePowerSave() throws IOException {
+		//get current thermostat info
+		Response info = getInfo();
 		
+		String data = "{\"target_temperature_f\":" + 65 + "}";
+		OkHttpClient client = new OkHttpClient();
+		MediaType mediaType = MediaType.parse("application/json");
+		RequestBody body = RequestBody.create(mediaType,data);
+		Request request = new Request.Builder()
+				.url("https://api.home.nest.com/oauth2/access_token")
+				.addHeader("Authorization", "Bearer " + accessToken)
+				.put(body)
+				.build();
+	}
+	
+	private Response getInfo() throws IOException {
+		//TODO: Incomplete
+		OkHttpClient client = new OkHttpClient();
+		Request request = new Request.Builder()
+				.url("https://developer-api.nest.com/structures")
+				.addHeader("Authorization","Bearer " + accessToken)
+				.get()
+				.build();
+		Response response = client.newCall(request).execute();
+		String response_str = response.body().string();
+		return null;
 	}
 }
